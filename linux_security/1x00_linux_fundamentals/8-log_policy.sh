9a719d5789ce07470fd1ac11d0c81259d476a76b
juleslgc <juleslgc@kali.com>
2026-08-05 10:18:31 +0200

    fix: again

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chown root:$2 "$1" && chmod 2750 $1 && chmod o-rwx "$1"
-cat << 'EOF' > /etc/logrotate.d/app
+cat << EOF > /etc/logrotate.d/app
 /var/log/app/*.log {
-create 0640 root www-data
+create 0640 root $2
 }
 EOF

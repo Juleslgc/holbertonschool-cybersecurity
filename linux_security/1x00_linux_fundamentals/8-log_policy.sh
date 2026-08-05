634398978d719815f465887351757200ba74006b
juleslgc <juleslgc@kali.com>
2026-08-05 10:05:19 +0200

    fix: added root on chown

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown :"$2" "$1" && chmod g+s "$1" && chmod o-rwx "$1"
+chown root:"$2" "$1" && chmod g+s "$1" && chmod o-rwx "$1"
 cat << 'EOF' > /etc/logrotate.d/app
 /var/log/app/*.log {
 create 0640 root www-data

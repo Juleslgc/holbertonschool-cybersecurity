5ed55ec3c6c97492f577dfb6c9ac4e2a9144d34a
juleslgc <juleslgc@kali.com>
2026-08-05 10:14:28 +0200

    fix: yet checker not accept syntax

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown root:$2 "$1" && chmod 2750 "$1" && chmod o-rwx "$1"
+chown root:$2 "$1" && chmod 2750 $1 && chmod o-rwx "$1"
 cat << 'EOF' > /etc/logrotate.d/app
 /var/log/app/*.log {
 create 0640 root www-data

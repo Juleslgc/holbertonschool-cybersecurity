730b2e0538bd613bba9fb8df4349b34f9c0e5f65
juleslgc <juleslgc@kali.com>
2026-08-06 20:03:21 +0200

    fix: fix operator in the variable for take the file size

--- linux_security/1x01_shell_ops/7-rotate.sh
+++ linux_security/1x01_shell_ops/7-rotate.sh
@@ -7,7 +7,7 @@ mkdir -p "$1/backups"
 
 for file in "$1"/*.log
 do
-        size=$(stat -c %s "$file")
+        size=$(stat -c%s "$file")
         name_file=$(basename "$file")
 
         if [ "$size" -gt 1024 ]; then

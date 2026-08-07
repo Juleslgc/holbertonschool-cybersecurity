ed6b65df5ba317b7c797a87db2274b10a92a31fc
juleslgc <juleslgc@kali.com>
2026-08-07 10:12:46 +0200

    fix: modified the code to keep the code on a single line

--- linux_security/1x02_identity_management/0-audit_uid.sh
+++ linux_security/1x02_identity_management/0-audit_uid.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-while  IFS=: read  -r f1 f2 f3 f4 f5 f6 f7
- do  
-	 if [ "$f3" -eq 0  ]; then
-		 echo "$f1"
-	 fi
-done  <  "$1"
+while  IFS=: read  -r f1 f2 f3 f4 f5 f6 f7; do [ "$f3" -eq 0  ] && echo "$f1"; done  <  "$1"

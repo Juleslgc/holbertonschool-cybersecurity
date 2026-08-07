7f24f103dcb1659e218b18adfef4f4ff9dae7f4b
juleslgc <juleslgc@kali.com>
2026-08-07 12:33:42 +0200

    fix: replace awk on the firt command

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-while IFS=: read -r user password uid gid rest
+awk -F: '$3 >= 1000 {users[$1]=1}' "$1"
+
+while read -r user
 do
-	if [ "$uid" -ge 1000  ]; then
-		for group in disk docker shadow
-		do
-			if awk -F: -v user="$user" -v group="$group" '$1==group && $4~"(^|,)" user "(,|$)"{
-				found=1
-			}
-		END {
-			exit !found
-		}' /etc/group; then
-			echo "$user:$group"
-			fi
-		done
-	fi
-done < "$1"
+    for group in disk docker shadow
+    do
+        if getent group "$group" | grep -qE "(:|,)$user(,|$)"; then
+            echo "$user:$group"
+        fi
+    done
+done < <(awk -F: '$3 >= 1000 {print $1}' "$1")

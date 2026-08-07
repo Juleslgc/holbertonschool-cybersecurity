48ae8a4b7f96554c06a06fb30be7a4bb6b2e5d6c
juleslgc <juleslgc@kali.com>
2026-08-07 12:30:31 +0200

    fix: deleted getent and grep for use awk

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -4,8 +4,13 @@ do
 	if [ "$uid" -ge 1000  ]; then
 		for group in disk docker shadow
 		do
-			if getent group "$group" | grep -qE "(:)$user(,|$)"; then
-				echo "$user:$group"
+			if awk -F: -v user="$user" -v group="$group" '$1==group && $4~"(^|,)" user "(,|$)"{
+				found=1
+			}
+		END {
+			exit !found
+		}' /etc/group; then
+			echo "$user:$group"
 			fi
 		done
 	fi

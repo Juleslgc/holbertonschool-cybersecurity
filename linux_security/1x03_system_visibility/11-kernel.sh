45b6f2f3f1d42fdf1befde319507825f5837fe17
juleslgc <juleslgc@kali.com>
2026-08-12 10:42:11 +0200

    fix: deleted " "

--- linux_security/1x03_system_visibility/11-kernel.sh
+++ linux_security/1x03_system_visibility/11-kernel.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -i "segfault" "$1" 2>/dev/null || grep -i "segfault" /var/log/messages 2>/dev/null
+grep -i segfault "$1" 2>/dev/null || grep -i segfault /var/log/messages 2>/dev/null

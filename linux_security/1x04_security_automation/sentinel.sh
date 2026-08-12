0e8956d613c8fe4d56d88d2e75e588199fe6a373
juleslgc <juleslgc@kali.com>
2026-08-12 18:53:34 +0200

    fix: added a function

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -9,3 +9,17 @@ fi
 if [ ! -v FILES_TO_WATCH ]; then
 	exit 1
 fi
+check_services() {
+for svc in "${SERVICES[@]}"
+do
+	if pgrep -f "$svc"; then
+		logger "OK: $svc is running"
+	else
+		if eval "$svc"; then
+			logger "FIXED: Restarted $svc"
+		else
+			logger "Error"
+		fi
+	fi
+done
+}

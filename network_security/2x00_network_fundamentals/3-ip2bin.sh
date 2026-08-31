a8387f3902f1dcd0a3042ec2338d5c25c99de19f
Juleslgc <venturajules99@gmail.com>
2026-08-31 16:01:23 +0200

    add: added a script then visualize an IP address as the machine sees it

---
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+printf "%08d.%08d.%08d.%08d\n" "$(echo "obase=2;$(echo "$1" | cut -d. -f1)" | bc)" "$(echo "obase=2;$(echo "$1" | cut -d. -f2)" | bc)" "$(echo "obase=2;$(echo "$1" | cut -d. -f3)" | bc)" "$(echo "obase=2;$(echo "$1" | cut -d. -f4)" | bc)"
